0d9f66b41f6c247e7fd8b44a9a414712e562d10a
Sergey Kondrashov <sergey.kondrashov@jetbrains.com>
2020-12-22 16:25:38 +0300

    add outputs

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,11 @@ output "alb_target_group_id" {
 output "alb_target_group_name" {
   value = aws_alb_target_group.this.name
 }
+
+output "alb_dns_name" {
+  value = aws_alb.this.dns_name
+}
+
+output "alb_dns_zone_id" {
+  value = aws_alb.this.zone_id
+}
